12263d7af6176f43c9510404465bad73bfbd6974
chann44 <44chansw@gmail.com>
2023-10-03 12:32:48 +0530

    added links of socials

--- utils/data.ts
+++ utils/data.ts
@@ -1,4 +1,10 @@
 import { ListItem } from "@/components/list-item";
 
+export const socialLinks = {
+  Github: "https://github.com/chann44",
+  Twitter: "https://twitter.com/44_chann",
+  Linkedin: "https://www.linkedin.com/in/vikash-manda-69a231200/",
+};
+
 export const projects: ListItem[] = [];
 export const work: ListItem[] = [];
